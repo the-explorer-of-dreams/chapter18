d29b37a63c70969764f6ddfbe22cc11312283014
william <wannabemyself@126.com>
2018-06-16 16:06:50 +0800

    service layer: 201806151606

--- src/main/java/com/smart/service/ForumService.java
+++ src/main/java/com/smart/service/ForumService.java
@@ -1,17 +1,12 @@
 package com.smart.service;
 
-import com.smart.dao.BoardDao;
-import com.smart.dao.PostDao;
-import com.smart.dao.TopicDao;
-import com.smart.dao.UserDao;
-import com.smart.domain.Board;
-import com.smart.domain.MainPost;
-import com.smart.domain.Topic;
-import com.smart.domain.User;
+import com.smart.dao.*;
+import com.smart.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ForumService {
@@ -41,6 +36,39 @@ public class ForumService {
     }
 
     /**
+     * add a new board
+     *
+     */
+    public void addBoard(Board board) {
+        boardDao.save(board);
+    }
+
+    /**
+     * remove a board
+     */
+    public void removeBoard(int boardId){
+        Board board = boardDao.get(boardId);
+        boardDao.remove(board);
+    }
+
+    /**
+     * 根据boardId获取Board对象
+     *
+     * @param boardId
+     */
+    public Board getBoardById(int boardId) {
+        return this.boardDao.get(boardId);
+    }
+
+    /**
+     * load all the boards
+     *
+     */
+    public List<Board> getAllBoard(){
+        return  this.boardDao.loadAll();
+    }
+
+    /**
      * add a topic,add a mainpost,the credits of the user +10,the topic number of the board +1
      */
     public void addTopic(Topic topic){
@@ -66,7 +94,147 @@ public class ForumService {
     }
 
     /**
-     *
+     * 更改主题帖
+     * @param topic
      */
-    /
+    public void updateTopic(Topic topic){
+        topicDao.update(topic);
+    }
+
+    /**
+     *remove the topic
+     */
+    public void removeTopic(int topicId) {
+        Topic topic = this.topicDao.get(topicId);
+
+        // 将论坛版块的主题帖数减1
+        Board board = boardDao.get(topic.getBoardId());
+        board.setTopicNum(board.getTopicNum() - 1);
+
+        // 发表该主题帖用户扣除50积分
+        User user = topic.getUser();
+        user.setCredit(user.getCredit() - 50);
+
+        // 删除主题帖及其关联的帖子
+        this.topicDao.remove(topic);
+        this.postDao.deleteTopicPosts(topicId);
+    }
+
+    /**
+     * make digest topic
+     */
+    public void makeDigestTopic(int topicId){
+        Topic topic = this.topicDao.get(topicId);
+        topic.setDigest(Topic.DIGEST_TOPIC);
+        User user = topic.getUser();
+        user.setCredit(user.getCredit() + 100);
+    }
+
+    /**
+     * get the paged topidcs of a board,order by reply time desc
+     */
+    public Page getPagedTopic(int boardId,int pageNo,int pageSize){
+        return this.topicDao.getPagedTopics(boardId,pageNo,pageSize);
+    }
+
+    /**
+     * 查找出所有包括标题包含title的主题帖
+     *
+     * @param title
+     *            标题查询条件
+     * @return 标题包含title的主题帖
+     */
+    public Page queryTopicByTitle(String title,int pageNo,int pageSize) {
+        return this.topicDao.queryTopicByTitle(title,pageNo,pageSize);
+    }
+    /**
+     * 根据topicId获取Topic对象
+     * @param topicId
+     * @return Topic
+     */
+    public Topic getTopicByTopicId(int topicId) {
+        return topicDao.get(topicId);
+    }
+
+
+    /**
+     * add a post:the credit of the user +5;the post number of the topic +1
+     * and refresh the last post time
+     */
+    public void addPost(Post post){
+        //add the post
+        this.postDao.save(post);
+        //the credit of the user +5
+        User user = post.getUser();
+        user.setCredit(user.getCredit() + 5);
+        this.userDao.update(user);
+        //the post number of the topic +1 and refresh the last post time
+        Topic topic = this.topicDao.get(post.getTopic());
+        topic.setTopicReplies(topic.getTopicReplies() + 1);
+        topic.setLastPost(new Date());
+        this.topicDao.update(topic);
+    }
+
+    /**
+     * remove a post:the credit of the user -20;the replies of the topic -1
+     */
+    public void removePost(int postId){
+        //remove a post
+        Post post = this.postDao.get(postId);
+        this.postDao.remove(post);
+        //the credit of the user -20
+        User user = post.getUser();
+        user.setCredit(user.getCredit() - 20);
+        this.userDao.update(user);
+        //the replies of the topic -1
+        Topic topic  = this.topicDao.get(post.getTopic());
+        topic.setTopicReplies(topic.getTopicReplies() - 1);
+        this.topicDao.update(topic);
+    }
+
+    /**
+     * 更改回复帖子的内容
+     * @param post
+     */
+    public void updatePost(Post post){
+        postDao.update(post);
+    }
+
+
+    /**
+     * get paged posts
+     */
+    public Page getPagedPosts(int topicId,int pageNo,int pageSize){
+        return this.postDao.getPagedPosts(topicId, pageNo, pageSize);
+    }
+
+
+    /**
+     * 获取回复帖子的对象
+     * @param postId
+     * @return 回复帖子的对象
+     */
+    public Post getPostByPostId(int postId){
+        return postDao.get(postId);
+    }
+
+
+    /**
+     * set the manager of the board
+     */
+    public void addBoardManager(int boardId,String userName){
+        User user = this.userDao.getUserByUserName(userName);
+        if(user == null){
+            throw new RuntimeException("user not existed");
+        }else{
+           Board board = this.boardDao.get(boardId);
+           user.getManBoards().add(board);
+           this.userDao.update(user);
+        }
+    }
+
+
+
+
+
 }
